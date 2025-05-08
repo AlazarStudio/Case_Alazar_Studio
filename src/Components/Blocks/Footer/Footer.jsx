71ea939c4c912b63a7e912f3c6e73c2cb1a5d27f
gonov <adamej10@gmail.com>
2025-05-08 16:44:06 +0300

    123

--- src/Components/Blocks/Footer/Footer.jsx
+++ src/Components/Blocks/Footer/Footer.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
 import classes from './Footer.module.css';
+import CenterBlock from '../../Standart/CenterBlock/CenterBlock';
+import WidthBlock from '../../Standart/WidthBlock/WidthBlock';
 
 function Footer({ children, ...props }) {
-    return ( 
-        <>
-            Footer
-        </>
-     );
+  return (
+    <>
+
+          <div className={classes.container}>
+            <span>©2024 ALAZAR STUDIO. ALL RIGHT RESERVED.</span>
+          </div>
+
+    </>
+  );
 }
 
 export default Footer;
